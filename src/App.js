e5886965848bf2d3aafc47b0cb0261ceb528ba11
cavapoo2 <forpetessake@protonmail.com>
2018-09-21 19:17:42 +0100

    add App code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import Lottery from './lotteryContainer';
+class App extends Component {
+  render() {
+    return (
+      <Lottery/>
+    );
+  }
+}
+
+export default App;
